4369076fd7a9833b962025284743b4d9fc0db0a8
McpeBooster <mcpebooster@gmail.com>
2017-05-14 19:06:17 +0200

    Added a little config

---
+++ src/SurvivalGames/SurvivalGames.php
@@ -0,0 +1,28 @@
+<?php
+namespace SurvivalGames;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+
+class SurvivalGames extends PluginBase implements Listener{
+	
+	const PREFIX = "§7[§2SurvivalGames§7]";
+	const TITLEPREFIX = "§2SurvivalGames";
+	
+	public static $instance;
+    public static $pfad;
+	
+	public function onEnable(){
+		$this->getLogger()->info(self::PREFIX . " by McpeBooster!");
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		self::$instance = $this;
+		$this->loadConfig();
+	}
+	
+	private function loadConfig(){
+		$this->saveResource("/config.yml");
+		@mkdir($this->getDataFolder()."maps");
+	}
+	
+}
